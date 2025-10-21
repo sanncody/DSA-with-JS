67f3565670d2c326e40fd1e829f93aec17cf5e41
Sandeep <sandeepwadhawan2001@gmail.com>
2025-10-22 00:24:47 +0530

    jagged array which has variable length subarrays is done

---
+++ Multi-Dimensional-Array/jaggedArray.js
@@ -0,0 +1,20 @@
+const prompt = require('./utils/promptSync');
+
+const outerArrSize = Number(prompt("Enter Outer array size: "));
+
+const arr = new Array(outerArrSize);
+
+for (let i = 0; i < arr.length; i++) {
+    const innerArrSize = Number(prompt("Enter Inner array size: "));
+    arr[i] = new Array(innerArrSize);
+    console.log();
+    for (j = 0; j < arr[i].length; j++) {
+        arr[i][j] = Number(prompt(`Enter row ${i + 1} col ${j + 1}: `));
+    }
+    console.log();
+}
+
+console.log();
+console.log(arr);
+
+
