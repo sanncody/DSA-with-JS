2d7ac1e6a67d79e49223e1e96282526fba582c32
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-12 20:15:57 +0530

    Palindromic String is done✅

--- Strings/palindromicString.js
+++ Strings/palindromicString.js
@@ -1,1 +1,17 @@
-const str = "Sandepe";
+const prompt = require('./utils/promptSync');
+
+const str = prompt("Enter a string: ");
+
+function isPalindrome(str) {
+    let start = 0, end = str.length - 1;
+
+    while (start <= end) {
+        if (str[start] !== str[end]) return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+console.log();
+isPalindrome(str) ? console.log("Palindromic String") : console.log("Non-Palindromic String");
