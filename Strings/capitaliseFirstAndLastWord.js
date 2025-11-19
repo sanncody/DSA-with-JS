2bfb3969cf66a751ed509342f5e2f0ccb8ed67bb
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-20 02:17:34 +0530

    Capitalising first and last alphabet of each word

---
+++ Strings/capitaliseFirstAndLastWord.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+
+const str = prompt("Enter a string: ");
+
+const strArr = str.split(' ');
+
+let resultantStr = "";
+for (let i = 0; i < strArr.length; i++) {
+    let word = strArr[i];
+    if (word.length <= 2) {
+        resultantStr += word.toUpperCase() + " ";
+    } else {
+        resultantStr += word.charAt(0).toUpperCase() 
+                        + word.substring(1, word.length - 1) 
+                        + word.charAt(word.length - 1).toUpperCase() + " ";
+    }
+}
+
+console.log(resultantStr);
