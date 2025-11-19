8ef0585933466c43f72fc02b76ed4880736edf4a
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-19 21:23:42 +0530

    Words includes prefix is done

---
+++ Strings/wordsContainPrefix.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+const size = Number(prompt("Enter number of words to be inserted in 'words' array: "));
+
+const words = new Array(size);
+
+console.log();
+for (let i = 0; i < words.length; i++) {
+    words[i] = prompt(`Enter word ${i + 1}: `);
+}
+console.log();
+
+const pref = prompt("Enter prefix which needs to checked in 'words' array: ");
+
+console.log();
+
+let count = 0;
+for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith(pref)) {
+        count++;
+    }
+}
+
+console.log();
+console.log(`Pref '${pref}' is coming ${count} times in [ ${words.join(', ')} ] array`);
