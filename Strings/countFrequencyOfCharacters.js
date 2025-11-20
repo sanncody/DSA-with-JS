6849ac99a32ecf216cdd79ad4ace58f04a44e2bd
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-20 16:29:54 +0530

    Count frequency of characters using bitmap

---
+++ Strings/countFrequencyOfCharacters.js
@@ -0,0 +1,19 @@
+// We have done this using Bitmap
+
+const prompt = require("./utils/promptSync");
+
+const str = prompt("Enter a string: ");
+
+let freq = new Array(123).fill(0);
+
+for (let i = 0; i < str.length; i++) {
+    const ascii = str.charCodeAt(i);
+    freq[ascii]++;
+}
+
+for (let i = 0; i < freq.length; i++) {
+    if (freq[i] > 0) {
+        console.log(`${String.fromCharCode(i)} -> ${freq[i]}`);
+    }
+}
+
