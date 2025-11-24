f48323d26850843f5ab3bb4fb2f1f6ed25e8a2bb
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-25 00:12:00 +0530

    Happy number with proper formatting is done✅

--- Set/HappyNumber.js
+++ Set/HappyNumber.js
@@ -1,23 +1,39 @@
-function isHappy(n) {
-    const set = new Set();
-    
-    while (true) {
-        let sum = 0;
-        while (n !== 0) {
-            let dig = n % 10;
-            sum += (dig * dig);
-            n = Math.floor(n / 10);
-        }
-        
-        if (sum === 1) return true;
-        if (set.has(sum)) return false;
-        
-        set.add(sum);
-        
-        n = sum;
+/*
+    A happy number is a number defined by the following process:
+
+    Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+    Those numbers for which this process ends in 1 are happy.
+    Return true if n is a happy number, and false if not.
+
+ */
+
+const prompt = require("./utils/promptSync");
+
+let n = Number(prompt("Enter a number: "));
+
+let set = new Set();
+
+let isHappy = false;
+while (true) {
+    let sum = 0;
+    while (n !== 0) {
+        let dig = n % 10;
+        sum += (dig * dig);
+        n = Math.floor(n / 10);
     }
-    
-    return false;
+
+    if (sum === 1) {
+        isHappy = true;
+        break;
+    }
+    if (set.has(sum)) {
+        isHappy = false;
+        break;
+    }
+
+    set.add(sum);
+    n = sum;
 }
 
-module.exports = { isHappy };
+isHappy ? console.log("Happy Number") : console.log("Non-Happy Number");
